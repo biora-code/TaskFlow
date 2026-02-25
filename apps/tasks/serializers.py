0e5decd7a8fe8c97a40ca3289e9d0c9020749a7a
biora-code <biora.merollari@gmail.com>
2026-02-25 10:16:36 +0100

    updated serializers

--- apps/tasks/serializers.py
+++ apps/tasks/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import Task
 
+
+class SubtaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'status', 'due_date', 'created_at']
+
+
 class TaskSerializer(serializers.ModelSerializer):
+    subtasks = SubtaskSerializer(many=True, read_only=True)
 
     class Meta:
         model = Task
